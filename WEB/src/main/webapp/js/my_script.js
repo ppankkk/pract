ee70be3021e3d954625401a4f08d63d0a964508a
ppankkk <ppankk@gmail.com>
2018-06-21 16:44:43 +0300

    Работает: 1. Простенькая веб страница (html+js+jQuery+Bootstrap) 2. Добавление, изменение, удаление, получение пользователей и продуктов
    Не работает:
    1. Не примененяется стиль из Bootstrap таблицы вывода пользователей/продуктов...
    2. Заказ. Не выполнено.
    
    Сделать:
    1. Заказ - полностью от таблиц до веб формы заказа (особо не начинал)
    2. Поправить стили таблицы (пофиксить пункт "не работает")
    3. Переходы по клику на строке таблицы в "карточку" пользователя/товара/заказа
    4. Авторизация и роли пользователь/работник/администратор.
    Пользователь должен уметь:
     - регистрироваться,
     - формировать заказы (новые),
     - добавлять товары к существующему заказу,
     - отправлять заказ на выполнение,
     - удалять себя из зарегистрированных пользователей, если нет активных заказов.
     Работник:
     - подтверждать регистрацию пользователей,
     - принимать/отклонять заказ полностью/частично с изменением статуса заказа,
     - добавлять/удалять/изменять товары.
     Администратор:
     - удалять/добавлять/изменять все что угодно.

--- WEB/src/main/webapp/js/my_script.js
+++ WEB/src/main/webapp/js/my_script.js
@@ -1,16 +1,13 @@
 $(document).ready(function () {
 
     $(".show_users").click(function () {
-        $('.dinamic-table').empty()
-        // $('.dinamic-table')
-        // $('.div-table').append("<table class=\"table table-striped dinamic-table\">");
-        $.getJSON("http://localhost:8080/users",function(response){
+        $.getJSON("/users",function(response){
             var tblHTML ='<tr><th>First Name</th><th>Last Name</th><th>Age</th></tr>';
             $.each(response, function(i, item){
-                tblHTML += '<tr><td>' + item.firstName + '</td><td>' + item.lastName + '</td><td>' + item.age + '</td></tr>';
+                tblHTML += '<tr><td>' + item.firstName + '</td><td>'
+                    + item.lastName + '</td><td>' + item.age + '</td></tr>';
             });
-            $('.dinamic-table').append(tblHTML);
-            // $('.dinamic-table').append('</table>');
+            $('.users-table').html(tblHTML);
         });
 
         $(".custom-container-users").removeClass('hide');
@@ -19,15 +16,16 @@ $(document).ready(function () {
     });
 
     $(".show_products").click(function () {
-        $('.dinamic-table').remove();
-        $('.div-table').append("<table class=\"table table-striped dinamic-table\">");
         $.getJSON("http://localhost:8080/products",function(response){
-            var tblHTML ='<tr><th>Product</th><th>Category</th><th>Color</th><th>Cout in warehouse</th><th>Price</th><th>Gender</th><th>Size</th></tr>';
+            var tblHTML ='<tr><th>Product</th><th>Category</th><th>Color</th><th>Cout in warehouse</th>'
+                           + '<th>Price</th><th>Gender</th><th>Size</th></tr>';
             $.each(response, function(i, item){
-                tblHTML += '<tr><td>' + item.name + '</td><td>' + item.category + '</td><td>' + item.color + '</td><td>' + item.count + '</td><td>' + item.price + '</td><td>' + item.gender + '</td><td>' + item.size + '</td></tr>';
+                tblHTML += '<tr><td>' + item.name + '</td><td>' + item.category + '</td><td>' + item.color
+                        + '</td><td>' + item.count + '</td><td>' + item.price + '</td><td>' + item.gender
+                        + '</td><td>' + item.size + '</td></tr>';
             });
-            $('.dinamic-table').append(tblHTML);
-            $('.dinamic-table').append('</table>');
+
+            $('.products-table').html(tblHTML);
         });
 
         $(".custom-container-users").addClass('hide');
@@ -36,6 +34,18 @@ $(document).ready(function () {
     });
 
     $(".show_orders").click(function () {
+        // $.getJSON("http://localhost:8080/orders",function(response){
+            var tblHTML ='<tr><th>Здесь будет отображение заказаов :)</th></tr>'/*'<tr><th>Product</th><th>Category</th><th>Color</th><th>Cout in warehouse</th>'
+                + '<th>Price</th><th>Gender</th><th>Size</th></tr>';*/
+            // $.each(response, function(i, item){
+                // tblHTML /*+= '<tr><td>' + item.name + '</td><td>' + item.category + '</td><td>' + item.color
+                //     + '</td><td>' + item.count + '</td><td>' + item.price + '</td><td>' + item.gender
+                //     + '</td><td>' + item.size + '</td></tr>';*/
+            // });
+
+            $('.orders-table').html(tblHTML);
+        // });
+
         $(".custom-container-users").addClass('hide');
         $(".custom-container-products").addClass('hide');
         $(".custom-container-orders").removeClass('hide');
