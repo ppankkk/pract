21050742d2408e606c11e9ebab31fdb08187f218
ppankkk <ppankk@gmail.com>
2018-08-29 17:05:46 +0300

    Added ability to add, delete and edit users via web forms with notifications.

--- WEB/src/main/webapp/js/my_script.js
+++ WEB/src/main/webapp/js/my_script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $(".show_users").click(function () {
+    $("#show_users").click(function () {
         $.getJSON("/users",function(response){
             var tblHTML ='<tr><th>First Name</th><th>Last Name</th><th>Age</th></tr>';
             $.each(response, function(i, item){
@@ -15,7 +15,7 @@ $(document).ready(function () {
         $(".custom-container-orders").addClass('hide');
     });
 
-    $(".show_products").click(function () {
+    $("#show_products").click(function () {
         $.getJSON("/products",function(response){
             var tblHTML ='<tr><th>Product</th><th>Category</th><th>Color</th><th>Cout in warehouse</th>'
                            + '<th>Price</th><th>Gender</th><th>Size</th></tr>';
@@ -33,7 +33,7 @@ $(document).ready(function () {
         $(".custom-container-orders").addClass('hide');
     });
 
-    $(".show_orders").click(function () {
+    $("#show_orders").click(function () {
         $.getJSON("/orders",function(response){
             var tblHTML ='<tr><th>Order #</th><th>User</th><th>Products in order</th>';
 
@@ -51,5 +51,61 @@ $(document).ready(function () {
         $(".custom-container-users").addClass('hide');
         $(".custom-container-products").addClass('hide');
         $(".custom-container-orders").removeClass('hide');
+    });
+
+    $("#addUser").click(function () {
+        $.ajax({
+            url: '/users',
+            type: 'POST',
+            dataType: 'text',
+            contentType: 'application/json',
+            data: JSON.stringify( { "firstName": $('#inputAddFirstName').val(),
+                "lastName": $('#inputAddSecoundName').val(),
+                "age": $('#inputAddAge').val()}),
+            success: function(data) {
+                Notify.generate('User added', 'Success', Notify.TYPE_SUCCESS);
+                document.getElementById('show_users').click();
+            },
+            error: function(data) {
+                Notify.generate('Error occurred', 'Error', Notify.TYPE_DANGER);
+            }
+        });
+    });
+
+    $("#deleteUserId").click(function () {
+        $.ajax({
+            url: '/users/' + $('#inputDeleteId').val(),
+            type: 'DELETE',
+            success: function(data) {
+                Notify.generate('User deleted', 'Success', Notify.TYPE_SUCCESS);
+                document.getElementById("show_users").click();
+            },
+            error: function(data) {
+                Notify.generate('Error occurred', 'Error', Notify.TYPE_DANGER);
+        }
+        });
+    });
+
+    $("#editUser").click(function () {
+
+        var userId = $('#inputEditId').val();
+
+        $.ajax({
+            url: '/users/' + userId,
+            type: 'PUT',
+            dataType: 'text',
+            contentType: 'application/json',
+            data: JSON.stringify( { "firstName": $('#inputEditFirstName').val(),
+                "lastName": $('#inputEditSecoundName').val(),
+                "age": $('#inputEditAge').val(),
+                "id": userId}),
+            success: function(data) {
+                Notify.generate('User updated', 'Success', Notify.TYPE_SUCCESS);
+                document.getElementById('show_users').click();
+            },
+            error: function(data) {
+                Notify.generate('Error occurred', 'Error', Notify.TYPE_DANGER);
+            }
+        });
     });
 });
